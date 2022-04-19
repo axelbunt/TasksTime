3125bbcb2d7e7ca59531474625798e32ac58048f
axelbunt <49002597+axelbunt@users.noreply.github.com>
2022-04-19 17:07:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,131 @@
+import datetime
+
+from flask import Flask, render_template, request, make_response, session
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask_restful import abort
+from werkzeug.utils import redirect
+
+from data import db_session
+from data.news import News
+from data.users import User
+from forms.tasks import TaskForm
+from forms.user import RegisterForm, LoginForm
+
+app = Flask(__name__)
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    days=365
+)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/')
+def index():
+    db_sess = db_session.create_session()
+    if current_user.is_authenticated:
+        news = db_sess.query(News).filter(
+            (News.user == current_user) | (News.is_private != True))
+    else:
+        news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect(f"/profile/{user.name}")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+
+        # wtf it doesn't work without it?
+        user.name = form.name.data
+        user.email = form.email.data
+        user.about = form.about.data
+
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/profile/<username>', methods=['GET', 'POST'])
+@login_required
+def show_profile(username: str):
+    form = TaskForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        task = News()
+        task.title = form.title.data
+        task.content = form.content.data
+        current_user.news.append(task)
+        db_sess.merge(current_user)
+        db_sess.commit()
+    return render_template('profile.html', username=username, form=form)
+
+
+@app.route('/news_delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def news_delete(id):
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.id == id,
+                                      News.user == current_user
+                                      ).first()
+    if news:
+        db_sess.delete(news)
+        db_sess.commit()
+    else:
+        abort(404)
+    return redirect('/')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+def main(db_sess=None):
+    db_session.global_init("db/blogs.db")
+    app.run()
+
+
+if __name__ == '__main__':
+    app.static_folder = 'static'
+    main()
